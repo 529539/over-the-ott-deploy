e80e5bab99c2ea36c72617f39e8a82ad0cdf5202
chaewon <89545462+gchaewon@users.noreply.github.com>
2022-08-04 16:19:34 +0900

    Fix: logo.svg 수정

--- TEST1/src/pages/Landing.js
+++ TEST1/src/pages/Landing.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../static/logo.svg';
+import { ReactComponent as Logo } from '../static/landingLogo.svg';
 import Background from '../components/Background';
 
 const Landing = () => {
@@ -29,13 +30,15 @@ const LadingWrapper = styled.div`
 	.logo {
 		//로고 아이콘 하단에 공백 없앤 파일 다시 받아 위치 수정
 		width: 100%;
+		border: solid;
 	}
 `;
 const ContentWrapper = styled.div`
+	border: solid;
 	position: absolute;
 	z-index: 2;
-	width: 717px;
-	height: 519px;
+	width: 37%; //717px;
+	height: 50%; //499px;
 	margin-left: 5%;
 	margin-top: 10%;
 	display: flex;
@@ -44,35 +47,36 @@ const ContentWrapper = styled.div`
 	justify-content: center;
 `;
 const ButtonWrapper = styled.div`
-	width: 667px;
-	height: 91px;
+	border: solid;
+	width: 100%;
+	height: 23%; //91px;
 	display: flex;
 	position: relative;
-	/* margin-bottom: 30px; */
+	margin-top: 9%;
 	justify-content: space-around;
 	button {
 		font-weight: 600;
 		font-size: 28px;
+		border-radius: 52.5px;
+		border-style: none;
+		opacity: 0.8;
+		box-shadow: 0px 2px 20px rgba(102, 102, 102, 0.3);
+	}
+	a {
+		width: 100%; //358px
+		height: 100%; //91px
 	}
 `;
 const LoginBtn = styled.button`
-	width: 280px;
+	width: 288px;
 	height: 81px;
 	background: #ffffff;
-	opacity: 0.8;
-	box-shadow: 0px 2px 20px rgba(102, 102, 102, 0.3);
-	border-radius: 52.5px;
-	border-style: none;
 	color: #d38189;
 `;
 const SignupBtn = styled.button`
-	width: 280px;
-	height: 81px;
+	width: 86%; //308px;
+	height: 100%; //91px;
 	background: #d38189;
-	opacity: 0.8;
-	box-shadow: 0px 2px 20px rgba(102, 102, 102, 0.3);
-	border-radius: 52.5px;
-	border-style: none;
 	color: #ffffff;
 `;
 export default Landing;
