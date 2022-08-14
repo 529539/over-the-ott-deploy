374beca2b5ff0f104980a4b707614c354bc91a44
서진 <dltjwls0529@gmail.com>
2022-08-15 04:08:42 +0900

    Feat: Checklist 인기 작품

--- TEST1/src/pages/Checklist.js
+++ TEST1/src/pages/Checklist.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Header from "../components/Header";
-import ChecklistSearchInput from "../components/checklist/ChecklistSearchInput";
+import Background from "../components/Background";
 import ChecklistTemplate from "../components/checklist/ChecklistTemplate";
 import ChecklistList from "../components/checklist/ChecklistList";
 import ChecklistDoneList from "../components/checklist/ChecklistDoneList";
@@ -14,16 +15,17 @@ const Checklist = () => {
 	return (
 		<>
 			<Header />
+			<Background />
 			<Wrapper>
 				<NotHeaderArea>
 					<div>
-						<ChecklistSearchInput
-							selected={selected}
-							input={input}
-							setSelected={setSelected}
-							setInput={setInput}
-						/>
-						<div style={{ display: "flex", justifyContent: "center" }}>
+						<div
+							style={{
+								display: "flex",
+								justifyContent: "center",
+								marginTop: "7vh",
+							}}
+						>
 							<ChecklistTemplate>
 								<ChecklistList />
 							</ChecklistTemplate>
@@ -31,6 +33,13 @@ const Checklist = () => {
 								<ChecklistDoneList />
 							</ChecklistTemplate>
 						</div>
+						<BtnWrapper>
+							<Link to="/checklist/search" style={{ textDecoration: "none" }}>
+								<Btn>
+									<BtnText>작품 검색 및 추가하러 가기</BtnText>
+								</Btn>
+							</Link>
+						</BtnWrapper>
 					</div>
 				</NotHeaderArea>
 			</Wrapper>
@@ -57,4 +66,28 @@ const NotHeaderArea = styled.div`
 	position: relative;
 	display: flex;
 	justify-content: center;
+`;
+
+const BtnWrapper = styled.div`
+	width: 100vw;
+	height: auto;
+	margin-top: 5vh;
+	display: flex;
+	justify-content: center;
+`;
+
+const Btn = styled.div`
+	width: 20vw;
+	height: 6vh;
+	background: rgba(255, 255, 255, 0.3);
+	box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.25);
+	border-radius: 1.5vw;
+`;
+
+const BtnText = styled.div`
+	font-weight: 600;
+	font-size: 1.2vw;
+	line-height: 6vh;
+	text-align: center;
+	color: #fff;
 `;
